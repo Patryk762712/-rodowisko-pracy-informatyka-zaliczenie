e432c67225c2752119f07d71071e81b06035a1fe
Patryk Modzelewski <patryk.modzelewski1221@gmail.com>
2025-01-19 17:17:40 +0100

    Dodanie tekstu "Second update"

--- FileInNewBranch.java
+++ FileInNewBranch.java
@@ -2,5 +2,6 @@ public class FileInNewBranch {
     public static void main(String[] args) {
         System.out.println("Hello in new branch");
         System.out.println("First update");
+        System.out.println("Second update");
     }
 }
